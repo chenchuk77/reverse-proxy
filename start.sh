73daeea8243935d597a67e8564eff1f82a0f6f9e
chenchuk77 <chenchuk@gmail.com>
2021-01-03 16:58:34 +0200

    fix start script

--- start.sh
+++ start.sh
@@ -1,1 +1,11 @@
-docker run -d --name apacheserver -p 90:80 -v $(readlink -f apacheconf/sites):/usr/local/apache2/conf/sites -v $(readlink -f apacheconf/htmlfiles):/usr/local/apache2/techolaf httpd-proxyenabled
+#!/bin/bash
+echo "starting reverse proxy [TCP/90] in the background ..."
+docker run -d --rm \
+       --name apacheserver \
+       -p 90:80 \
+       -v $(readlink -f apacheconf/sites):/usr/local/apache2/conf/sites \
+       -v $(readlink -f apacheconf/htmlfiles):/usr/local/apache2/techolaf \
+       httpd-proxyenabled
+
+
+#docker run -d --name apacheserver -p 90:80 -v $(readlink -f apacheconf/sites):/usr/local/apache2/conf/sites -v $(readlink -f apacheconf/htmlfiles):/usr/local/apache2/techolaf httpd-proxyenabled
