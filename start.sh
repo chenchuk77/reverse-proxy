3de763c3d50c29a6e4c655f88b4c87facd8cbe00
chenchuk77 <chenchuk@gmail.com>
2021-01-04 15:44:27 +0200

    exposing ssl

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ echo "starting reverse proxy [TCP/90] in the background ..."
 docker run -d --rm \
        --name apacheserver \
        -p 90:80 \
+       -p 993:443 \
        -v $(readlink -f apacheconf/sites):/usr/local/apache2/conf/sites \
        -v $(readlink -f apacheconf/htmlfiles):/usr/local/apache2/techolaf \
        httpd-proxyenabled
